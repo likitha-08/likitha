3b0650660f047f29e85a34fe58320049cdbf9906
likitha-08 <likitha0405hyd@gmail.com>
2022-01-31 17:41:46 +0530

    logic for if else

---
+++ demo1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int age;
+			Console.Write("Enter age of a person:");
+			age = Convert.ToInt32(Console.ReadLine());
+
+			if (age >= 18)
+			{
+				Console.WriteLine("person is allowed for voting:" + age);
+			}
+			else
+			{
+				Console.WriteLine("User is not allowed for voting:" + age);
+			}
+		}
+    }
+}
+
