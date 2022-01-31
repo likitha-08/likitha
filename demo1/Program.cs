fd5ab6e862d5112460372a3f097e5aeee540dbd5
likitha-08 <likitha0405hyd@gmail.com>
2022-01-31 18:00:02 +0530

    greatest of three

--- demo1/Program.cs
+++ demo1/Program.cs
@@ -10,19 +10,32 @@ namespace demo1
     {
         static void Main(string[] args)
         {
-			int age;
-			Console.Write("Enter age of a person:");
-			age = Convert.ToInt32(Console.ReadLine());
+            int num1, num2, num3;
+            Console.Write("Find the largest of three numbers:\n");
 
-			if (age >= 18)
-			{
-				Console.WriteLine("person is allowed for voting:" + age);
-			}
-			else
-			{
-				Console.WriteLine("User is not allowed for voting:" + age);
-			}
-		}
+            Console.Write("Input the 1st number :");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the  2nd number :");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the 3rd  number :");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("The 1st Number is the greatest among three.");
+                }
+                else
+                {
+                    Console.Write("The 3rd Number is the greatest among three.");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("The 2nd Number is the greatest among three ");
+            else
+                Console.Write("The 3rd Number is the greatest among three ");
+        }
     }
-}
+ }
 
