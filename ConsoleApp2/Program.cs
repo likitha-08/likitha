aca57adae5399c3bf67eefefaad3fe03c46c18ae
likitha-08 <likitha0405hyd@gmail.com>
2022-02-01 10:09:47 +0530

    iterative statements and loops

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+            Console.Write("sum of n natural numbers:\n");
+
+            Console.Write("enter the no of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nThe first {0} natural number are :\n", n);
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", i);
+                sum += i;
+            }
+            Console.Write(" \n Sum of Natural Number upto {0} terms : {1} \n", n, sum);
+        }
+    }
+}
