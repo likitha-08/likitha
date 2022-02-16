ed5c7b1779c9b42d6ecd199fd79fc365c8a3fd94
likitha-08 <likitha0405hyd@gmail.com>
2022-02-16 16:21:09 +0530

    sql demo

---
+++ SQLQuery.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE Student 
+(Rollno varchar(30),Name varchar(30),gender varchar(200),class varchar(100))
+
+select * from Student
+insert into Student values('402','likitha','female','ECE-A')
+insert into Student values('403','priya','female','ECE-A')
+insert into Student values('405','rahul','male','ECE-A')
+
+create table course(courseid int,coursename varchar(100))
+select * from course
+insert into course values('4031','datastructures')
+insert into course values('4022','dotnet')
+insert into course values('4562','java')
+insert into course values('4786','embedded systems')
