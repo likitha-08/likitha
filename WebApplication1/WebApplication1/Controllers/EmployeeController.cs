b89b4dd952025015e39a6b28641a165ff5eede0a
likitha-08 <likitha0405hyd@gmail.com>
2022-02-24 17:47:27 +0530

    web apllication

---
+++ WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    { 
+        IList< Employee > employees = new List< Employee >()
+        {
+            new Employee()
+                { EmployeeId = 1, EmployeeName = "Rahul", EmployeeAge =31, EmployeeDOB = 04-05-1996 },
+            new Employee()
+                { EmployeeId = 1, EmployeeName = "Jhon", EmployeeAge = 32, EmployeeDOB = 04-05-1996 },
+            new Employee()
+                { EmployeeId = 1, EmployeeName = "Riya", EmployeeAge = 30, EmployeeDOB = 04-05-1996 },
+        };
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return employees;
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Model/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+        public int EmployeeDOB { get; set; }
+
+    }
+}
