64cbf893e81495f8526150cfc73c39b0e14ce464
likitha-08 <likitha0405hyd@gmail.com>
2022-03-08 16:58:31 +0530

    case study

---
+++ WebApiWithEF/WebApiWithEF/Models/HRMSContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebApiWithEF.Models
+{
+    public partial class HRMSContext : DbContext
+    {
+        public HRMSContext()
+        {
+        }
+
+        public HRMSContext(DbContextOptions<HRMSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TbIemployee> TbIemployees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=HRMS;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TbIemployee>(entity =>
+            {
+                entity.ToTable("tbIEmployee");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.CreatedOn).HasColumnType("date");
+
+                entity.Property(e => e.EmployeeId)
+                    .HasMaxLength(50)
+                    .HasColumnName("EmployeeID");
+
+                entity.Property(e => e.EmployeeName).HasMaxLength(50);
+
+                entity.Property(e => e.UpdatedOn).HasColumnType("date");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
